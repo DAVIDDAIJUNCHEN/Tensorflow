a91e3bd771461ec0e68c98118d5f50072d68d1f1
daijun_chen <daijun.chen@nuance.com>
2019-02-02 06:02:44 -0500

    add loss and epoch list

--- Logistic_Regression/train.py
+++ Logistic_Regression/train.py
@@ -13,7 +13,8 @@ display_step = 5
 
 with tf.Session() as sess:
     sess.run(init) 
-    plotdata = {'batchsize:': [], 'loss:': []}
+    plotdata = {'batchsize': [], 'loss': []}
+    LOSS = []
 
     for epoch in range(training_epochs):
         for (x, y) in zip(Train_X, Train_Y):
@@ -21,14 +22,18 @@ with tf.Session() as sess:
             sess.run(optimizer, feed_dict=feed)
             
         if epoch % display_step == 0:
-            feed = {X:x, Y:y}
+            feed = {X:Train_X, Y:Train_Y}
             loss1 = sess.run(loss, feed_dict=feed)
+            LOSS.append(sess.run(loss, feed_dict={X:Train_X, Y:Train_Y}))
+            plotdata['batchsize'].append(epoch)
             w1 = sess.run(w)
             b1 = sess.run(b)
-            print('Epoch: {},  Loss: {}, w={} and b={}'.format(epoch+1, loss1, w1, b1))
+            print('Epoch: {},  Loss: {}, w= {} and b= {}'.format(epoch+1, loss1, w1, b1))
+
+    plotdata['loss'] = LOSS
+    w2 = sess.run(w)
+    b2 = sess.run(b)
     print('Training finished!')
     print('Loss = ', sess.run(loss, feed_dict={X:Train_X, Y:Train_Y}), 'w=', sess.run(w), 'b=', sess.run(b))
-
-
 
 
