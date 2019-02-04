6a9b0af2db2e25502261a93daf8f8115c8ee6e2b
daijun_chen <daijun.chen@nuance.com>
2019-02-03 23:09:25 -0500

    add loss function

---
+++ MLP/loss_function.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+
+class LOSS(object):
+    def __init__(self, input):
+        self.input = input
+
+    def relu():
+        return tf.max(self.input, 0)
+
