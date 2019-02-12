c5a275c399e54b91a8bb06fd06e44b953fcf459d
daijun_chen <daijun.chen@nuance.com>
2019-02-12 07:45:35 -0500

    modify train_model.py

--- RNN/ASR_BiRNN/train_model.py
+++ RNN/ASR_BiRNN/train_model.py
@@ -41,44 +41,45 @@ with tf.Session() as sess:
 
         n_batches_per_epoch = int(np.ceil(len(labels) / batch_size))
         
-    print('total_loop ', n_batches_per_epoch, 'in one epoch, ', batch_size, ' items in one loop')
+        print('total_loop ', n_batches_per_epoch, 'in one epoch, ', batch_size, ' items in one loop')
     
-    train_loss = 0.0
-    train_label_error_rate = 0.0
-    next_idx = 0
+        train_loss = 0.0
+        train_label_error_rate = 0.0
+        next_idx = 0
     
-    for batch in range(n_batches_per_epoch):
-        next_idx, source, source_lengths, sparse_labels = next_batch(labels, next_idx, batch_size)
-        feed = {input_tensor:source, targets:sparse_labels, seq_length:source_lengths,
+        for batch in range(n_batches_per_epoch):
+            next_idx, source, source_lengths, sparse_labels = next_batch(labels, next_idx, batch_size)
+            feed = {input_tensor:source, targets:sparse_labels, seq_length:source_lengths,
                                                             keep_dropout:keep_dropout_rate}
-        batch_loss, _ = sess.run([avg_loss, optimizer], feed_dict=feed)
-        train_loss += batch_loss
+            batch_loss, _ = sess.run([avg_loss, optimizer], feed_dict=feed)
+            train_loss += batch_loss
     
-        if (batch+1)%20 == 0:
-            print('loop: ', batch, 'train cost:', train_loss/(batch+1))
-            feed2 = {input_tensor: source, targets: sparse_labels,seq_length: source_lengths,keep_dropout:1.0}
+            if (batch+1)%20 == 0:
+                print('loop: ', batch, 'train cost:', train_loss/(batch+1))
+                feed2 = {input_tensor: source, targets: sparse_labels,seq_length: source_lengths,keep_dropout:1.0}
 
-            d,train_ler = sess.run([decoded[0],label_error_rate], feed_dict=feed2)
-            dense_decoded = tf.sparse_tensor_to_dense( d, default_value=-1).eval(session=sess)
-            dense_labels = sparse_tuple_to_texts_ch(sparse_labels, words)
+                d,train_ler = sess.run([decoded[0],label_error_rate], feed_dict=feed2)
+                dense_decoded = tf.sparse_tensor_to_dense( d, default_value=-1).eval(session=sess)
+                dense_labels = sparse_tuple_to_texts_ch(sparse_labels, words)
             
-            counter =0
-            print('Label err rate: ', train_ler)
-            for orig, decoded_arr in zip(dense_labels, dense_decoded):
-            # convert to strings
-                decoded_str = ndarray_to_text_ch(decoded_arr,words)
-                print(' file {}'.format( counter))
-                print('Original: {}'.format(orig))
-                print('Decoded:  {}'.format(decoded_str))
-                counter=counter+1
-                break
-                
-    epoch_duration = time.time() - epoch_start
+                counter =0
+                print('Label err rate: ', train_ler)
+                for orig, decoded_arr in zip(dense_labels, dense_decoded):
+                # convert to strings
+                    decoded_str = ndarray_to_text_ch(decoded_arr,words)
+                    print(' file {}'.format( counter))
+                    print('Original: {}'.format(orig))
+                    print('Decoded:  {}'.format(decoded_str))
+                    counter = counter + 1
+                    break
+                            
+        epoch_duration = time.time() - epoch_start
     
-    log = 'Epoch {}/{}, train_cost: {:.3f}, train_ler: {:.3f}, time: {:.2f} sec'
-    print(log.format(epoch ,epochs, train_loss,train_ler,epoch_duration))
-    saver.save(sess, save_dir+"asr.cpkt", global_step=epoch)
+        log = 'Epoch {}/{}, train_cost: {:.3f}, train_ler: {:.3f}, time: {:.2f} sec'
+        print(log.format(epoch ,epochs, train_loss,train_ler,epoch_duration))
+        saver.save(sess, save_dir+"asr.cpkt", global_step=epoch)
     
 train_duration = time.time() - train_start
 print('Training complete, total duration: {:.2f} min'.format(train_duration / 60))
 
+    
