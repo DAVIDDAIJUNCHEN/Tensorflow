a9eb0631c612972357cc14a211651e31d8978d89
daijun_chen <daijun.chen@nuance.com>
2019-02-17 22:10:51 -0500

    update the model with accuracy evaluation

--- RNN/RNN_LM/model.py
+++ RNN/RNN_LM/model.py
@@ -7,14 +7,12 @@ Module task: build model
 """
 
 import tensorflow as tf
-from data import words_len
+from data import words_size
 
 #------------------------------- Build RNN_LM model --------------------------#
 # define hyper parameters #
 learning_rate = 0.001
-training_iters = 10000
-display_step = 1000
-n_input = 4  # input seq length 
+n_input = 4   # input seq length 
 
 n_hidden1 = 256
 n_hidden2 = 512
@@ -22,7 +20,7 @@ n_hidden3 = 512
 
 # define placeholder #
 x = tf.placeholder('float32', [None, n_input, 1]) # 4 consective words 
-wordy = tf.placeholder('float32', [None, words_len]) # one-hot representation
+wordy = tf.placeholder('float32', [None, words_size]) # one-hot representation
 
 # define network structure #
 x1 = tf.reshape(x, [-1, n_input]) # (, 4)
@@ -35,11 +33,11 @@ LSTMCell3 = tf.contrib.rnn.LSTMCell(n_hidden3)
 rnn_cell = tf.contrib.rnn.MultiRNNCell([LSTMCell1, LSTMCell2, LSTMCell3]) # define cell
 
 outputs, states = tf.contrib.rnn.static_rnn(cell=rnn_cell, inputs=x2, dtype=tf.float32)
-pred = tf.contrib.layers.fully_connected(outputs[1], words_len, activation_fn=None)
+pred = tf.contrib.layers.fully_connected(outputs[1], words_size, activation_fn=None)
 
 # define optimizer #
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=wordy))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).miniminze(loss)
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
 # evaluate model #
 correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(wordy, 1))
